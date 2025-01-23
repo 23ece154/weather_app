6a13a8c96dfe3eccc7a39abcd5a89e4b674c5f5c
23ece154 <srikarthika.l2023ece@sece.ac.in>
2025-01-24 02:10:52 +0530

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>  { /*mount the weather com from weather.jsx */} 
+    </div>
+  )
+}
+
+export default App
